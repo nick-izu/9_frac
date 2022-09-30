cf1a4f5093f6f7d740291b8223511f8282fb12cd
nick-izu <51707671+nick-izu@users.noreply.github.com>
2022-09-30 14:39:34 +0200

    Update run_flow.py

--- run_flow.py
+++ run_flow.py
@@ -1,64 +1,70 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep 10 12:04:03 2020
-@author: rosie
-"""
+from pydfnworks import *
 
-import os, sys
-from time import time
-from pydfnworks import * 
-from numpy import *
-
-def rewrite_file(filename,b,three=False):
-    with open(filename,'r') as f:
-        lines = f.readlines()
-    
-    j = 0
-    header = True
-    s = ' '
-    for line in lines:
-        if header:
-            header = False
-        else:
-            values = line.strip().split()
-            if three:
-                values[-3] = str(b[j])
-                values[-2] = str(b[j])
-                
-            values[-1] = str(b[j]) + '\n'
-    
-            j = j + 1
-            lines[j] = s.join(values)
-    
-    with open(filename,'w') as f:
-        for line in lines:
-            f.writelines(line)
-        
-        
-define_paths()
-main_time = time()
 DFN = create_dfn()
 
-DFN.set_flow_solver("PFLOTRAN")
-DFN.dfn_gen(output=True)
+##dfnGen
+DFN.make_working_directory()
+DFN.check_input()
+DFN.create_network()
+DFN.mesh_network(visual_mode=False)
 
-b = zeros(DFN.num_frac)
-b[:3]= 1e-3
-b[3] = 5e-4 #change the aperture of the fourth fracture
+porosity = 1.0
+tortuosity= 1.0
 
-cwd = os.getcwd()
-##############
-#perm
+#Layer = 1
+variable = "transmissivity"
+function = "correlated"
+params = {"alpha":2.2*10**-9, "beta":0.8}
+b1,perm1,T1 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
 
-perm = b**2/12 # calculate permeability as function of aperture
+function = "correlated"
+params = {"alpha":3.5*10**-9, "beta":0.9}
+b1,perm2,T2 = DFN.generate_hydraulic_values(variable,function,params,family_id=2)
 
-############
-#dump into file
-filename_aper = cwd + '/aperture.dat'
-filename_perm = cwd + '/perm.dat'
+# Then we combine them.
+T = T1 + T2 + T3  
+b = b1 + b2 + b3 
+perm = perm1 + perm2 + perm3 
 
-rewrite_file(filename_aper,b)
-rewrite_file(filename_perm,perm,three=True)
+# Entires 0-3, should be zero here. 
+print(b[0:3])
 
+# JDH- now assigne each of those values for aperture
+b[0] = 1*10**-3 
+b[1] = 1*10**-3 
+b[2] = 1*10**-3
+b[3] = 1*10**-3 
+
+
+# convert perm using the cubic law
+perm[0] = b[0]**2/12
+perm[1] = b[1]**2/12
+perm[2] = b[2]**2/12
+perm[3] = b[3]**2/12
+
+
+
+# determine values of Transmissivity for determinsitc fractures
+mu = 8.9e-4  #dynamic viscosity of water at 20 degrees C, Pa*s
+g = 9.8  #gravity acceleration
+rho = 1000  # water density
+
+T[0] = (b[0]**3 * rho * g) / (12 * mu)
+T[1] = (b[1]**3 * rho * g) / (12 * mu)
+T[2] = (b[2]**3 * rho * g) / (12 * mu)
+T[3] = (b[3]**3 * rho * g) / (12 * mu)
+
+DFN.dump_hydraulic_values(b,perm,T)
+
+# Add transmissivity values to the mesh for visualization
+DFN.add_variable_to_mesh("trans,", "transmissivity.dat", "full_mesh.inp")
+
+## JDH I added an exit here for you to look at the files before you run the simulation.
+#exit()
+
+#dfnFlow
 DFN.dfn_flow()
+
+
+#dfntrans
+DFN.dfn_trans()
